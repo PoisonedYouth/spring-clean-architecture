bf9dd6c452a1758a66497c708305db6bcb00b765
Matthias Schenk <matthias.schenk@sevdesk.de>
2023-05-21 13:13:52 +0200

    [V-1-0] Initial commit

---
+++ src/test/kotlin/com/poisonedyouth/springcleanarchitecture/SpringCleanArchitectureApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.poisonedyouth.springcleanarchitecture
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringCleanArchitectureApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
